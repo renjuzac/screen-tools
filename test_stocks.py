63da84cb9f6a35b5ee48d473928458953ded5be2
renjuzac <renjuzac@gmail.com>
2018-06-12 23:07:21 -0700

    tests for scan

--- test_stocks.py
+++
@@ -1,8 +0,0 @@
-from stocks import stocks
-
-
-def test_get_stocks_with_growth_over():
-
-    assert type(stocks.get_stocks_with_revenue_growth_over("0.3")) == dict
-    print(len(stocks.get_stocks_with_revenue_growth_over("0.1")))
-    assert (len(stocks.get_stocks_with_revenue_growth_over("0.1")) > 0) == True
